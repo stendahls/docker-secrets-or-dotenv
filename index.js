3284647f368ffaff1c8ba833714f6dab22a3c899
Oskar Risberg <oskar.risberg@stendahls.se>
2021-06-28 11:10:19 +0200

    load from the right folder when module

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const path = require('path');
 module.exports = function read(secretName) {
 
     if(process.env.NODE_ENV !== 'production'){
-        console.log(`Loading ${secretName} from ${path.join(__dirname, '..', '.env')}`);
+        console.log(`Loading ${secretName} from ${path.join('.', '.env')}`);
 
-        const iniData = fs.readFileSync(path.join(__dirname, '..', '.env'), 'utf8');
+        const iniData = fs.readFileSync(path.join('.', '.env'), 'utf8');
         const vars = Object.fromEntries(iniData.split('\n').map(varValue => varValue.split('=')));
 
         return vars[secretName];
